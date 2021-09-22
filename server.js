39e88b897dcee68ab9f61bd5586161213d778e6f
Burak Turhan <trhnburak@gmail.com>
2021-09-22 11:24:42 +0300

    --

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const jsonServer = require('json-server');
+const express = require('express');
+const cors = require('cors');
+
 const server = jsonServer.create();
 const router = jsonServer.router('items.json');
 const router2 = jsonServer.router('companies.json');
+const app = express();
+
+app.use(cors())
 
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(router, router2);
-
+server.use(router);
+server.use(router2);
 
 
 server.listen(port);
