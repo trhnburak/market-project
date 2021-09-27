d3a9235fb6d27a091b3dd95dee68339c461e0abf
Burak Turhan <trhnburak@gmail.com>
2021-09-27 16:02:27 +0300

    --

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const jsonServer = require('json-server');
 
 const server = jsonServer.create();
 const router = jsonServer.router('items.json');
+const router2 = jsonServer.router('companies.json');
 
 
 const middlewares = jsonServer.defaults();
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+server.use(router2);
 
 
 server.listen(port);
