8d4da2764b80114db8839f01a63c0d8a532279fc
Burak Turhan <trhnburak@gmail.com>
2021-09-27 16:01:44 +0300

    -try to fix network error on heroku

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const jsonServer = require('json-server');
 
 const server = jsonServer.create();
 const router = jsonServer.router('items.json');
-const router2 = jsonServer.router('companies.json');
 
 
 const middlewares = jsonServer.defaults();
@@ -11,7 +10,6 @@ const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
-server.use(router2);
 
 
 server.listen(port);
