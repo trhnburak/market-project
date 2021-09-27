63c645da89e817433048f58f9be824b4b373094e
Burak Turhan <trhnburak@gmail.com>
2021-09-27 16:16:21 +0300

    --

--- frontend/build/precache-manifest.ee9541c22f93b45c7ffc2ad576290b76.js
+++ frontend/build/precache-manifest.ee9541c22f93b45c7ffc2ad576290b76.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "034ed6d81b181af44a65a72e424f5482",
+    "revision": "55f3025d8cdfdda8d17cee13cda55369",
     "url": "/index.html"
   },
   {
-    "revision": "85d20f28f600ec4200b0",
+    "revision": "946c217c4db6671199e1",
     "url": "/static/css/main.1e4dcf32.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.e88d36fe.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "85d20f28f600ec4200b0",
-    "url": "/static/js/main.02d22172.chunk.js"
+    "revision": "946c217c4db6671199e1",
+    "url": "/static/js/main.da9d4cb8.chunk.js"
   },
   {
     "revision": "2747b914e9a60db2276f",
